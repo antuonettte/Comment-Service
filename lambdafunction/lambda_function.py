a7939a07051fe4298dd99283d27ab1110fe2c067
Antonio Turner <tonyturner1399@gmail.com>
2024-06-28 06:47:48 -0400

    Moving Variables

--- lambdafunction/lambda_function.py
+++ lambdafunction/lambda_function.py
@@ -1,41 +1,86 @@
 import json
 import pymysql
+import logging
+import os
 
 # Database configuration
-db_host = 'car-network-db.c5kgayasi5x2.us-east-1.rds.amazonaws.com'
-db_user = 'admin'
-db_password = 'FrostGaming1!'
-db_name = 'comment_db'
+DB_HOST = os.environ['DB_HOST']
+DB_USER = os.environ['DB_USER']
+DB_PASSWORD = os.environ['DB_PASSWORD']
+COMMENT_DB_NAME = os.environ['COMMENT_DB_NAME']
 
 # Connect to the database
 conn = pymysql.connect(
-    host=db_host,
-    user=db_user,
-    password=db_password,
-    database=db_name
+    host=DB_HOST,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    database=COMMENT_DB_NAME
 )
 cursor = conn.cursor()
+
+#Logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 
 
 def lambda_handler(event, context):
     # Determine the HTTP method
-    http_method = None
     http_method = event['httpMethod']
- 
-
+    path = event.get('resource')
+    
     if http_method == 'GET':
         # Handle GET request
         post_id = event['queryStringParameters']['post_id']
         return get_comments_by_post(post_id)
     
     elif http_method == 'POST':
-        # Handle POST request
-        comment_data = json.loads(event['body'])
+        if path == '/comments/sql':
+            conn = pymysql.connect(
+                host=DB_HOST,
+                user=DB_USER,
+                password=DB_PASSWORD,
+                database=event['queryStringParameters'].get('database')
+            )
+            cursor = conn.cursor()
+            try:
+                cursor.execute(event['queryStringParameters'].get('query'))
+                conn.commit()
+                
+                return {
+                    'statusCode':200,
+                    'headers': {
+                      "Access-Control-Allow-Origin": "*", 
+                      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+                      "Access-Control-Allow-Credentials": 'true',
+                    },
+                    'body':json.dumps({'message':cursor.fetchall()})
+                    }
+            except Exception as e:
+                conn.rollback()
+                return {
+                    'statusCode':500,'headers': {
+                      "Access-Control-Allow-Origin": "*", 
+                      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+                      "Access-Control-Allow-Credentials": 'true',
+                    },
+                    'body':json.dumps({'error':str(e)})
+                }
+                
+        elif path == '/comments' :
+            # Handle POST request
+            logger.info("posting comment")
+            comment_data = json.loads(event['body'])
         return post_comment(comment_data)
     
     else:
         return {
             'statusCode': 400,
+            'headers': {
+                      "Access-Control-Allow-Origin": "*", 
+                      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+                      "Access-Control-Allow-Credentials": 'true',
+                    },
             'body': json.dumps(event)
         }
     
@@ -50,26 +95,47 @@ def get_comments_by_post(post_id):
 
             return{
                 'statusCode' : 200,
+                'headers': {
+                      "Access-Control-Allow-Origin": "*", 
+                      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+                      "Access-Control-Allow-Credentials": 'true',
+                    },
                 'body' : json.dumps(convert_tuple_to_dict(comments))
             }
         else:
             return {
                 'statusCode':404,
+                'headers': {
+                      "Access-Control-Allow-Origin": "*", 
+                      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+                      "Access-Control-Allow-Credentials": 'true',
+                    },
                 'body': json.dumps('No comments for post ID')
             }
     except Exception as e:
         return {
             'statusCode':500,
+            'headers': {
+                      "Access-Control-Allow-Origin": "*", 
+                      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+                      "Access-Control-Allow-Credentials": 'true',
+                    },
             'body' : json.dumps(f'Error: {str(e)}')
         }
 
 
 def post_comment(comment_data):
     try:
-        cursor.execute("INSERT INTO comments (post_id, user_id, content) VALUES (%s, %s, %s)", (comment_data['post_id'], comment_data['user_id'], comment_data['content']))
+        logger.info(comment_data)
+        cursor.execute("INSERT INTO comments (post_id, user_id, username, content) VALUES (%s, %s, %s, %s)", (comment_data['post_id'], comment_data['user_id'], comment_data['username'], comment_data['content']))
         conn.commit()
         return {
             'statusCode': 201,
+            'headers': {
+                      "Access-Control-Allow-Origin": "*", 
+                      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+                      "Access-Control-Allow-Credentials": 'true',
+                    },
             'body': json.dumps('Comment created successfully')
         }
 
@@ -77,6 +143,11 @@ def post_comment(comment_data):
         conn.rollback()
         return {
             'statusCode': 500,
+            'headers': {
+                      "Access-Control-Allow-Origin": "*", 
+                      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+                      "Access-Control-Allow-Credentials": 'true',
+                    },
             'body': json.dumps(f'Error: {str(e)}')
         }
     
@@ -88,6 +159,7 @@ def convert_tuple_to_dict(comments):
             "post_id":comment[1],
             "user_id":comment[2],
             "content":comment[3],
+            "username":comment[5],
             "created_at":comment[4].strftime('%Y-%m-%d %H:%M:%S')
         })
 
